101293588812edb459f7c3e99caba950b21eb569
Oğuz Ergül <erguloguz@hotmail.com>
2020-09-13 14:54:16 +0300

    film components has been created

---
+++ src/components/Card/sectionCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="custom">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"SectionCard",
+  props:["title"]
+}
+</script>
+
+<style scoped>
+.custom {
+  display: flex;
+  min-height: 500px;
+  display: flex;
+  padding: 20px;
+  background-color: rgba(var(--darker));
+  margin-top: 10px;
+
+}
+</style>
