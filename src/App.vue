3d013896ef3da24cfe8872f041e20e966655e629
Oğuz Ergül <erguloguz@hotmail.com>
2020-09-20 22:44:23 +0300

    icons removed and some routes changed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <Header/>
+
     <Container>
       <router-view/>
     </Container>
+
   </div>
 </template>
 <script>
+
 import Container from "./components/Container";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+
 export default {
-  components: {Footer, Header, Container}
+  components: {
+    Header,
+    Container
+  },
+
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,36 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import VuexPersistence from "vuex-persist";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
-
-const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
-  key: "film-app",
-});
-import movies from "../store/movies/movies";
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {
-    movies,
+
+  state: {
+    Movies: [],
+    favorites: [],
   },
-  plugins: [vuexLocal.plugin],
-});
+  mutations: {
+    SET_MOVIES(state, movies) {
+      state.Movies = movies;
+    },
+    SET_FAVORITE(state, movie) {
+      state.favorites.push(movie);
+    },
+    REMOVE_FAVORITE(state, movie) {
+      const index = state.favorites.findIndex((item) => item.imdbID === movie.imdbID);
+      state.favorites.splice(index, 1);
+    }
+  },
+  actions: {
+    searchMovie({commit}, searchText) {
+      const axios = require('axios');
+      axios.get('http://www.omdbapi.com/?=tt3896198&apikey=6a71067&s=' + searchText).then((response) => {
+          commit('SET_MOVIES', response.data.Search)
+      })
+    },
+  },
+  getters: {
+    getFavorites: (state) => state.favorites,
+  },
+
+})
